2f4909bf0e07a912937f16bb65c96a8a0eb88aa8
Decave <decave27@gmail.com>
2021-03-26 21:15:04 +0900

    feat: add basic client

---
+++ gitscript/client.py
@@ -0,0 +1,18 @@
+import asyncio
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class Client:
+    def __init__(self, **options):
+        self.token = options.get("username", None)
+        self.token = options.get("token", None)
+        self.httpsession = options.get("httpsession", None)
+        if token:
+            self.login(username, token=token)
+
+    def _repr(self):
+        if self.session.auth:
+            return "<GitHub [{!r}]>".format(self.httpsession.auth)
+        return "<Anonymous GitHub at 0x{0:x}>".format(id(self))
