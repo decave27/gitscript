977c9d4b3b35985c526ea5f8ffab57e0a204be05
Decave <decave27@gmail.com>
2021-03-28 22:42:10 +0900

    fix: repr

--- gitscript/client.py
+++ gitscript/client.py
@@ -12,7 +12,9 @@ class Client:
         if token:
             self.login(username, token=token)
 
-    def _repr(self):
+    def __repr__(self):
         if self.session.auth:
             return "<GitHub [{!r}]>".format(self.httpsession.auth)
         return "<Anonymous GitHub at 0x{0:x}>".format(id(self))
+
+    
